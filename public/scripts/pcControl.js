86403da89e99f1dd6749464b88e296aa1f0d766d
Pavel Zelenko <43108877+paxom4ik4@users.noreply.github.com>
2020-03-25 23:19:19 +0300

    update2

--- public/scripts/pcControl.js
+++ public/scripts/pcControl.js
@@ -9,32 +9,51 @@ let gameFieldStyle = window.getComputedStyle(gameField);
 let bodyStyles = window.getComputedStyle(document.body);
 let playerStyles = window.getComputedStyle(player);
 
-    // Функция, вызываемая после зажатия ЛКМ (mousedown)
-    function mouseDownEvent() {
-        addEventListener("mousemove", gameMouse); // Вызывает gameMouse() при движении мыши (mousemove)
-        addEventListener("mousedown", createBullet); 
-        
-    }
+// Функция, вызываемая после зажатия ЛКМ (mousedown)
+function mouseDownEvent() {
+    addEventListener("mousemove", gameMouse); // Вызывает gameMouse() при движении мыши (mousemove)
+}
 
-    // Функция, вызываемая после отжатия ЛКМ (mouseUp)
-    function mouseUpEvent() {
-        removeEventListener("mousemove", gameMouse); // Сбрасывает событие, вызываемое движением мыши (mousemove)
-        
-        // Мягко говоря, эта функция останавливает движение объекта #player
-    }
-    // Функция стрельбы (создаем элементы div при нажатии через определенный интервал)
-    function createBullet(){
-        let bullet = document.createElement("div");
-        bullet.classList.add("bullet"); 
-        gameField.append(bullet);
-    }
+// Функция, вызываемая после отжатия ЛКМ (mouseUp)
+function mouseUpEvent() {
+    removeEventListener("mousemove", gameMouse); // Сбрасывает событие, вызываемое движением мыши (mousemove)
+    // Мягко говоря, эта функция останавливает движение объекта #player
+}
 
+/****************************************************/
+/************ События и функции для пуль ************/
+let bulTimer;
+function createBullet() {
+
+    let bullet = document.createElement("div");
+    bullet.setAttribute("id", "bullet");
+
+    let bulletStyles = window.getComputedStyle(bullet);
+    let matrix = new WebKitCSSMatrix(bulletStyles.webkitTransform);
+    gameField.append(bullet);
+    bullet.style.transform = "translateX(" + matrix.m41 + "px)";
+    
+
+    
+}
+
+function setBulletInterval() {
+    bulTimer = setInterval(createBullet, 500);
+}
+
+function removeBulletInterval() {
+    bulTimer = clearInterval(bulTimer);
+    //removeEventListener("mousedown", setBulletInterval);
+}
+
+/****************************************************/
+/****************************************************/
 // Главная функция, вызываемая после зажатия ЛКМ и движении мыши (mouseDown + mousemove)
 function gameMouse(e) {
     let leftField = parseInt(gameFieldStyle.marginLeft) + 25; // Определяем размер пустого пространства слева
     let rightField = parseInt(bodyStyles.width) - leftField * 2; // Размер слева
 
-    let matrix = new WebKitCSSMatrix(playerStyles.webkitTransform); // Вытаскиваем из стиля "transform" инфу о расположении #player (translateX)
+    // let matrix = new WebKitCSSMatrix(playerStyles.webkitTransform); // Вытаскиваем из стиля "transform" инфу о расположении #player (translateX)
 
     //alert(matrix.m41);
 
@@ -57,6 +76,6 @@ function gameMouse(e) {
 addEventListener("mousedown", mouseDownEvent);
 addEventListener("mouseup", mouseUpEvent);
 
-// gameField.addEventListener('click', createBullet);
-
+addEventListener("mousedown", setBulletInterval);
+addEventListener("mouseup", removeBulletInterval);
 
